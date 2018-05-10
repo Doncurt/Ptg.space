01553d41c9d4cd75ab7ac6cd6d2dac591c55404f
Donovan Adams <donovan.adams@students.makeschool.com>
2018-05-10 13:28:34 -0700

    got the front page working

--- server.js
+++ server.js
@@ -12,8 +12,15 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.render('home', { msg: 'Hello World!' });
 })
-
-
+app.get('/process',(req,res)=>{
+  res.render('process');
+})
+app.get('/about',(req,res)=>{
+  res.render('about');
+})
+app.get('/deliverables',(req,res)=>{
+  res.render('deliverables');
+})
 app.listen(process.env.PORT || 3000, ()=>{
   console.log('PTG Museum on port 3000');
 })
