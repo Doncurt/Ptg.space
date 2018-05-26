993c8eb1beb608c69a2943a8f9972c7f6e3610bc
Donovan Adams <donovan.adams@students.makeschool.com>
2018-05-26 06:31:21 -0700

    acknowledgements

--- models/wikiarticles.js
+++ models/wikiarticles.js
@@ -2,41 +2,32 @@ module.exports = [
     {
         title:"1782 Buntebart Square Piano (London)",
         link:"http://my.ptg.org/foundation/museum/pianos/buntebart",
+        img:"https://higherlogicdownload.s3.amazonaws.com/PTG/d76a3fa5-1de9-4b09-96d5-6319711ec378/UploadedImages/pianos/Buntebart/3BuntebartFront1.jpg",
     },
     {
         title:"Laura Keene's Piano",
         link:"http://my.ptg.org/foundation/museum/pianos-on-exhibit/lincoln-chickering",
+        img:"https://daily.jstor.org/wp-content/uploads/2015/04/Laura_Keene.jpg",
     },
     {
         title:"The Historical Piano at Ford’s Theatre",
         link:"http://my.ptg.org/foundation/museum/pianos-on-exhibit/lincoln-chickering",
+        img:"https://higherlogicdownload.s3.amazonaws.com/PTG/d76a3fa5-1de9-4b09-96d5-6319711ec378/UploadedImages/Lincoln%20Chickering%20-1.jpg",
     },
     {
         title:"What is a Registered Piano Technician (RPT)?",
         link:'http://www.ptg.org/document//121/',
+        img:"http://www.mitchellpianoservice.com/RPT%20Logo.jpg",
     },
     {
         title:"Soufleto 'bridge' piano",
-        link:"http://my.ptg.org/foundation/museum/pianos/soufleto"
+        link:"http://my.ptg.org/foundation/museum/pianos/soufleto",
+        img:"https://higherlogicdownload.s3.amazonaws.com/PTG/d76a3fa5-1de9-4b09-96d5-6319711ec378/UploadedImages/pianos/Soufleto/1SoufletoFullClosedsm.jpg",
     },
     {
        title:"The Piano Technicians Guild ",
         link:"http://www.ptg.org/scripts/4disapi.dll/4DCGI/cms/review.html?Action=CMS_Document&DocID=18&MenuKey=Menu6",
+        img:"/img/piano-technicians-guild-logo.png",
     },
-    {
-        title:"Title 7",
-        body:"If you buy a lion's mane for this dog, then people wont know that you don't have a pet lion."
-    },
-    {
-       title:"Title 8",
-        body:"If dogs ruled the earth, there wouldn't be any wars."
-    },
-    {
-       title:"Title 9",
-        body:"If dogs ruled the earth, there wouldn't be any wars."
-    },
-    {
-       title:"Title 10",
-        body:"If dogs ruled the earth, there wouldn't be any wars."
-    },
+
 ];
